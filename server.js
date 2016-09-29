1f1ce34bf3fde22e40ad64c06da6460f03f65eb7
William McDonald <ginoskotheon@yahoo.com>
2016-09-29 14:02:05 -0500

    Update
    server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var http = require('http');
 var express = require("express");
 var app = express();
 var path = require('path');
-
+var Search = require('bing.search');
 
 app.set('port', (process.env.PORT || 8080));
-
 
 
 
@@ -15,51 +14,61 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.route('/latest')
 
+    .get(getRecent);
 
- app.get('/:url*', function (req, res){
-    var home = "https://sleepy-savannah-67922.herokuapp.com/"
-    var url = req.url.slice(1);
+app.get('/:query', function (req, res){
+    var query = req.params.query;
+    var offset = req.query.offset || 10;
+    var search = new Search(process.env.API_KEY);
     var result = {};
     if(validateURL(url)){
         result = {
-            "URL": url,
-            "Hobbit URL": home + linkGen()
+            "Term": query,
+            "Date": new Date().toLocaleString();
         };
-        // res.send(result);
-        res.send(
-        "<html>" +
-        "<head><title>Request URL Microservice</title></head>" +
-        "<body>" +
-        "<h1>Request Header Parser</h1>" +
-       "<p> URL: " + JSON.stringify(result["URL"]) + "</p>" +
-       "<p>Hobbit URL: <a href='"+ url +"'>"+ JSON.stringify(result["Hobbit URL"]) +  "</a></p>" +
-        "</body>" +
-        "</html>"
-        );
-    }
-    else {
-        result = {
-        "error": "No hobbits here. Make sure the URL has 'https://' proceeding it."
-     
-        };
-        res.send(result);
 
-    }  
+
+        search.images(query, {
+            top: size
+        },
+        function(err, results) {
+            if(err) throw err;
+            res.send(results.map(makeList));
+        }
+        
+        
+        );
+        // res.send(result);
+    //     res.send(
+    //     "<html>" +
+    //     "<head><title>Request URL Microservice</title></head>" +
+    //     "<body>" +
+    //     "<h1>Request Header Parser</h1>" +
+    //    "<p> URL: " + JSON.stringify(result["URL"]) + "</p>" +
+    //    "<p>Hobbit URL: <a href='"+ url +"'>"+ JSON.stringify(result["Hobbit URL"]) +  "</a></p>" +
+    //     "</body>" +
+    //     "</html>"
+    //     );
+    }
+ 
 });
 
 
-function linkGen() {
-    var num = Math.floor(100000 + Math.random() * 900000);
-    return num.toString().substring(0, 4);
+function makeList(img) {
+    return {
+        "URL": img.url,
+        "Snippet": img.title,
+        "Thumbnail": img.thumbnail.url,
+        "Context": img.sourceUrl
+    };
 }
 
-function validateURL(url) {
-    // Checks to see if it is an actual url
-    // Regex from https://gist.github.com/dperini/729294
-    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-    return regex.test(url);
-  }
+function getRecent(req, res) {
+    History 
+
+}
 
 app.listen(app.get('port'), function(){
     console.log('Listening on port', app.get('port'));
